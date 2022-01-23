29f07172a29ad9fba884346706174307cdbb5fad
Marcos Alcozer <marcos@alcozer.dev>
2022-01-23 19:37:44 +0000

    Corrects nesting structure

--- project_dbt/models/staging/edfi/stg_edfi_sessions.sql
+++ project_dbt/models/staging/edfi/stg_edfi_sessions.sql
@@ -17,11 +17,13 @@ WITH parsed_data AS (
         PARSE_DATE('%Y-%m-%d', JSON_VALUE(data, "$.beginDate")) AS begin_date,
         PARSE_DATE('%Y-%m-%d', JSON_VALUE(data, "$.endDate")) AS end_date,
         ARRAY(
-            SELECT AS STRUCT 
-                SPLIT(JSON_VALUE(grading_periods, '$.gradingPeriodReference.gradingPeriodDescriptor'), '#')[OFFSET(1)] AS grading_period_descriptor,
-                CAST(JSON_VALUE(grading_periods, "$.gradingPeriodReference.periodSequence") AS int64) AS period_sequence,
-                JSON_VALUE(grading_periods, "$.gradingPeriodReference.schoolId") AS school_id,
-                JSON_VALUE(grading_periods, "$.gradingPeriodReference.schoolYear") AS school_year
+            SELECT AS STRUCT
+                STRUCT(
+                    SPLIT(JSON_VALUE(grading_periods, '$.gradingPeriodReference.gradingPeriodDescriptor'), '#')[OFFSET(1)] AS grading_period_descriptor,
+                    CAST(JSON_VALUE(grading_periods, "$.gradingPeriodReference.periodSequence") AS int64) AS period_sequence,
+                    JSON_VALUE(grading_periods, "$.gradingPeriodReference.schoolId") AS school_id,
+                    JSON_VALUE(grading_periods, "$.gradingPeriodReference.schoolYear") AS school_year
+                ) AS grading_period_reference
             FROM UNNEST(JSON_QUERY_ARRAY(data, "$.gradingPeriods")) grading_periods 
         ) AS grading_periods
     FROM {{ source('staging', 'base_edfi_sessions') }}
