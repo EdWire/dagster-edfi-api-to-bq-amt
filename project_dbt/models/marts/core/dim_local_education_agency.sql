70eec7ee33b7134e07f4c26c089ea6d9f50787d0
Marcos Alcozer <marcos@alcozer.dev>
2022-01-23 18:32:32 +0000

    Hashes dim_local_education_agency

--- project_dbt/models/marts/core/dim_local_education_agency.sql
+++ project_dbt/models/marts/core/dim_local_education_agency.sql
@@ -6,6 +6,8 @@
 
 
 SELECT DISTINCT
-    local_education_agency_id AS local_education_agency_key,
-    name_of_institution AS local_education_agency_name
+    {{ dbt_utils.surrogate_key([
+        'local_education_agency_id'
+    ]) }}                               AS local_education_agency_key,
+    name_of_institution                 AS local_education_agency_name
 FROM {{ ref('stg_edfi_local_education_agencies') }}
