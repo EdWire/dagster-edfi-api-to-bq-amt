8efe4c65f3b0ed78d43fa74c5d27f09221e14fe6
Marcos Alcozer <marcos@alcozer.dev>
2022-01-26 18:18:16 +0000

    Documents macros

--- project_dbt/macros/convert_grade_level_to_id.sql
+++ project_dbt/macros/convert_grade_level_to_id.sql
@@ -1,23 +1,21 @@
--- this macro converts grade level descriptors
--- to a numeric value for use in sorting in
--- a bi tool
+
 {% macro convert_grade_level_to_id(grade_level) %}
-    CASE
-        WHEN {{ grade_level }} = 'Infant/toddler' THEN -3
-        WHEN {{ grade_level }} = 'Preschool/Prekindergarten' THEN -2
-        WHEN {{ grade_level }} = 'Kindergarten' THEN -1
-        WHEN {{ grade_level }} = 'First grade' THEN 1
-        WHEN {{ grade_level }} = 'Second grade' THEN 2
-        WHEN {{ grade_level }} = 'Third grade' THEN 3
-        WHEN {{ grade_level }} = 'Fourth grade' THEN 4
-        WHEN {{ grade_level }} = 'Fifth grade' THEN 5
-        WHEN {{ grade_level }} = 'Sixth grade' THEN 6
-        WHEN {{ grade_level }} = 'Seventh grade' THEN 7
-        WHEN {{ grade_level }} = 'Eighth grade' THEN 8
-        WHEN {{ grade_level }} = 'Ninth grade' THEN 9
-        WHEN {{ grade_level }} = 'Tenth grade' THEN 10
-        WHEN {{ grade_level }} = 'Eleventh grade' THEN 11
-        WHEN {{ grade_level }} = 'Twelfth grade' THEN 12
+    CASE {{ grade_level }}
+        WHEN 'Infant/toddler'            THEN -3
+        WHEN 'Preschool/Prekindergarten' THEN -2
+        WHEN 'Kindergarten'              THEN -1
+        WHEN 'First grade'               THEN 1
+        WHEN 'Second grade'              THEN 2
+        WHEN 'Third grade'               THEN 3
+        WHEN 'Fourth grade'              THEN 4
+        WHEN 'Fifth grade'               THEN 5
+        WHEN 'Sixth grade'               THEN 6
+        WHEN 'Seventh grade'             THEN 7
+        WHEN 'Eighth grade'              THEN 8
+        WHEN 'Ninth grade'               THEN 9
+        WHEN 'Tenth grade'               THEN 10
+        WHEN 'Eleventh grade'            THEN 11
+        WHEN 'Twelfth grade'             THEN 12
         ELSE 999999999
     END
 {% endmacro %}
--- project_dbt/macros/convert_grade_level_to_short_label.sql
+++ project_dbt/macros/convert_grade_level_to_short_label.sql
@@ -1,23 +1,21 @@
--- this macro converts grade level descriptors
--- to shorted, often numeric grade levels
--- Fifth grade --> 5
+
 {% macro convert_grade_level_to_short_label(grade_level) %}
-    CASE
-        WHEN {{ grade_level }} = 'Infant/toddler' THEN 'Infant'
-        WHEN {{ grade_level }} = 'Preschool/Prekindergarten' THEN 'PreK'
-        WHEN {{ grade_level }} = 'Kindergarten' THEN 'K'
-        WHEN {{ grade_level }} = 'First grade' THEN '1'
-        WHEN {{ grade_level }} = 'Second grade' THEN '2'
-        WHEN {{ grade_level }} = 'Third grade' THEN '3'
-        WHEN {{ grade_level }} = 'Fourth grade' THEN '4'
-        WHEN {{ grade_level }} = 'Fifth grade' THEN '5'
-        WHEN {{ grade_level }} = 'Sixth grade' THEN '6'
-        WHEN {{ grade_level }} = 'Seventh grade' THEN '7'
-        WHEN {{ grade_level }} = 'Eighth grade' THEN '8'
-        WHEN {{ grade_level }} = 'Ninth grade' THEN '9'
-        WHEN {{ grade_level }} = 'Tenth grade' THEN '10'
-        WHEN {{ grade_level }} = 'Eleventh grade' THEN '11'
-        WHEN {{ grade_level }} = 'Twelfth grade' THEN '12'
+    CASE {{ grade_level }}
+        WHEN 'Infant/toddler'            THEN 'Infant'
+        WHEN 'Preschool/Prekindergarten' THEN 'PreK'
+        WHEN 'Kindergarten'              THEN 'K'
+        WHEN 'First grade'               THEN '1'
+        WHEN 'Second grade'              THEN '2'
+        WHEN 'Third grade'               THEN '3'
+        WHEN 'Fourth grade'              THEN '4'
+        WHEN 'Fifth grade'               THEN '5'
+        WHEN 'Sixth grade'               THEN '6'
+        WHEN 'Seventh grade'             THEN '7'
+        WHEN 'Eighth grade'              THEN '8'
+        WHEN 'Ninth grade'               THEN '9'
+        WHEN 'Tenth grade'               THEN '10'
+        WHEN 'Eleventh grade'            THEN '11'
+        WHEN 'Twelfth grade'             THEN '12'
         ELSE '999999999'
     END
 {% endmacro %}
